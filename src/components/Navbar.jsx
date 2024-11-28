1608067f9c3692db1b19a0dc01834ee48a45ad3f
Oghenevwede Louis Umukoro <wedevilleg@gmail.com>
2024-11-28 18:02:19 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from "react"
+import Link from "next/link"
+import Image from "next/image"
+
+
+const Navbar=()=>{
+    return(
+        <>
+            <div>
+                <h1>This is Navbar</h1>
+            </div>
+        </>
+    )
+
+
+
+};
+export default Navbar;
